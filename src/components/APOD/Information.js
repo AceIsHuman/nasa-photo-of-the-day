fbc23f7a46b8d7c27a8672b33ff838369e94ff40
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-08-01 21:52:51 -0500

    use semantic-ui-react Header

--- src/components/APOD/Information.js
+++ src/components/APOD/Information.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Container } from "semantic-ui-react";
+import { Container, Header } from "semantic-ui-react";
 
 import Date from './Date';
 
 export default function(props) {
     return(
         <Container text textAlign="left" >
-            <h2>{props.title}</h2>
-            <h3>{props.copyright ? `Copyright: ${props.copyright}` : 'This Photo is Public Domain'}</h3>
+            <Header as='h2'>{props.title}</Header>
+            <Header as='h3'>{props.copyright ? `Copyright: ${props.copyright}` : 'This Photo is Public Domain'}</Header>
             <p>{props.explanation}</p>
             <Date date={props.date} setDate={props.setDate}/>
         </Container>
