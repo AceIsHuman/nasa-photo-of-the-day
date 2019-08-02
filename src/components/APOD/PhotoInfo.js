2786fc3663ee6321f4ccf22824962a68b09e2950
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-08-01 21:12:36 -0500

    move location of date

--- src/components/APOD/PhotoInfo.js
+++ src/components/APOD/PhotoInfo.js
@@ -5,8 +5,8 @@ import Information from "./Information";
 export default function(props) {
     return (
         <div className="photo-info">
-            <Date date={props.date} setDate={props.setDate} />
             <Information copyright={props.copyright} title={props.title} explanation={props.explanation} />
+            <Date date={props.date} setDate={props.setDate} />
         </div>
     )
 }
