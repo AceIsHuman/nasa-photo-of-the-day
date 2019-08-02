d86f391fb5742dab84353870c20c53ea0a72059e
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-08-01 21:48:27 -0500

    move date into Information componenet

--- src/components/APOD/Information.js
+++ src/components/APOD/Information.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
 
+import Date from './Date';
+
 export default function(props) {
     return(
         <Container text textAlign="left" >
             <h2>{props.title}</h2>
             <h3>{props.copyright ? `Copyright: ${props.copyright}` : 'This Photo is Public Domain'}</h3>
             <p>{props.explanation}</p>
+            <Date date={props.date} setDate={props.setDate}/>
         </Container>
     )
 }
