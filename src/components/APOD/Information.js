6c4a451fb425dcee0e20d5cca67dc8b458e2f89b
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-08-01 21:01:47 -0500

    add text container

--- src/components/APOD/Information.js
+++ src/components/APOD/Information.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Container } from "semantic-ui-react";
 
 export default function(props) {
     return(
-        <div>
+        <Container text>
             <h2>{props.title}</h2>
             <h3>Copyright: {props.copyright}</h3>
             <p>Explanation: {props.explanation}</p>
-        </div>
+        </Container>
     )
 }
