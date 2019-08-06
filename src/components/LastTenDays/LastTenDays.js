8985299b55d66557ecbfba073fd132073308d90c
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-08-06 13:29:56 -0500

    fix bug in reduceDate function

--- src/components/LastTenDays/LastTenDays.js
+++ src/components/LastTenDays/LastTenDays.js
@@ -11,23 +11,24 @@ export default function() {
         const stringSplit = date.split('-');
         const dateSplit = stringSplit.map(num => parseInt(num))
         let [year, month, day] = [dateSplit[0], dateSplit[1], dateSplit[2]];
-        
+      
         function n(n){
-            return n > 9 ? "" + n: "0" + n;
+          return n > 9 ? "" + n: "0" + n;
         }
-        
+      
         if (month === 1 && day === 1) { year--; month = 12; day = 31 }
         else if (month === 3 && day === 1) {month--; day = 28}
-        else if (month < 9 && month % 2 === 1) {month--; day = 30}
-        else if (month < 9 && month % 2 === 0) {month--; day = 31}
-        else if (month > 8 && month % 2 === 1) {month--; day = 31}
-        else if (month > 8 && month % 2 === 0) {month--; day = 30}
+        else if (month < 9 && month % 2 === 1 && day === 1) {month--; day = 30}
+        else if (month < 9 && month % 2 === 0 && day === 1) {month--; day = 31}
+        else if (month > 8 && month % 2 === 1 && day === 1) {month--; day = 31}
+        else if (month > 8 && month % 2 === 0 && day === 1) {month--; day = 30}
+      
         else if (day > 1) {day--} 
-        
+      
         month = n(month); day = n(day);
-        
+      
         return `${year}-${month}-${day}`;
-    }
+      }
 
     const moment = require('moment');
     let startDate = moment().format('YYYY-MM-DD');
