4af3f4df1e5355f424b5d4eb00778f389d9463a5
AceIsHuman <menoman1995@yahoo.com>
2020-11-18 18:17:09 -0600

    add margin above title

--- src/components/APOD/Information.js
+++ src/components/APOD/Information.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { Container, Header } from "semantic-ui-react";
 
-import Date from './Date';
-
 export default function(props) {
     return(
-        <Container text textAlign="left" >
+        <Container text textAlign="left" style={{ marginTop: '1rem' }}>
             <Header as='h2'>{props.title}</Header>
             <Header as='h3'>{props.copyright ? `Copyright: ${props.copyright}` : 'This Photo is Public Domain'}</Header>
             <p>{props.explanation}</p>
-            <Date date={props.date} setDate={props.setDate}/>
         </Container>
     )
 }
